c13f4172d031dce7a65c0f013d25f01e47f545da
DmitryTL <Dmitry.plus1@yandex.ru>
2025-11-22 23:54:47 +0600

    fix

---
+++ src/main/java/org/LukDT/task_manager_api/repository/UserRepository.java
@@ -0,0 +1,13 @@
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+  //Используется, при аутентификации: нужно получить пользователя по email.
+  Optional<User> findByEmail(String email);
+
+  //Автоматически задает вопрос, есть ли хотя бы один пользователь с таким email
+  boolean existsByEmail(String email);
+}
