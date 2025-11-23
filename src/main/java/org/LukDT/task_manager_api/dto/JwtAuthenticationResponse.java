90fd9b6f6a52a2187224190b53028108ea986636
DmitryTL <Dmitry.plus1@yandex.ru>
2025-11-23 14:34:51 +0600

    add JwtAuthenticationResponse

---
+++ src/main/java/org/LukDT/task_manager_api/dto/JwtAuthenticationResponse.java
@@ -0,0 +1,13 @@
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Ответ с токеном доступа")
+public class JwtAuthenticationResponse {
+  @Schema(description = "Токен", example = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTYyMjUwNj...")
+  private String token;
+}
