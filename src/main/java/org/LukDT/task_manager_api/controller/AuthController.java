92d370ccb13457c503a87a66fcf925ff55ff8c68
DmitryTL <Dmitry.plus1@yandex.ru>
2025-12-01 13:58:54 +0600

    created basic registration and login methods

--- src/main/java/org/LukDT/task_manager_api/controller/AuthController.java
+++ src/main/java/org/LukDT/task_manager_api/controller/AuthController.java
@@ -1,3 +1,8 @@
+import org.LukDT.task_manager_api.service.UserService;
+import org.LukDT.task_manager_api.dto;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +12,17 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-  
+  private final UserService userService;
+
+  @PostMapping("/register")
+  public ResponseEntity<UserResponse> register(@RequestBody RegisterRequest request) {
+    UserResponse response = userService.register(request);
+    return ResponseEntity.ok(response);
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<JwtAuthentication> login(@RequestBody LoginRequest request) {
+    JwtAuthentication response = userService.login(request);
+    return ResponseEntity.ok(response);
+  }
 }
