f4bd703c8be61fff579a6873781ab83f38447660
DmitryTL <Dmitry.plus1@yandex.ru>
2025-12-04 15:42:11 +0600

    fix imports in role model

--- src/main/java/org/LukDT/task_manager_api/model/Role.java
+++ src/main/java/org/LukDT/task_manager_api/model/Role.java
@@ -2,11 +2,18 @@ import java.util.HashSet;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+
 import lombok.Getter;
+import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
@@ -16,20 +23,26 @@ import lombok.Setter;
 @NoArgsConstructor
 @Table(name="roles")
 public class Role {
+
+  public enum RoleType {
+    ROLE_USER,
+    ROLE_ADMIN
+  }
+
   @Id
   @Column(name="id")
   @GeneratedValue(strategy=GenerationType.IDENTITY)
   @Setter(AccessLevel.NONE)
   private Long id;
 
-  @Column(name="name")
+  @Enumerated(EnumType.STRING)
+  @Column(name="name", nullable=false)
   private String name;
   
   @ManyToMany(mappedBy="roles", fetch=FetchType.LAZY)
-  private Set<User> users;
+  private Set<User> users = new HashSet<>();
 
   public Role(String name) {
     this.name = name;
-    users = new HashSet<>();
   }
 }
