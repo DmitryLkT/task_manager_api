9d84a30239a0b0778a6fe265ed0b82668b400c28
DmitryTL <Dmitry.plus1@yandex.ru>
2025-12-05 19:48:01 +0600

    add tables projects and tasks

--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    email varchar(250) NOT NULL UNIQUE
+    email VARCHAR(250) NOT NULL UNIQUE
                   CHECK (email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
     password_hash VARCHAR(255) NOT NULL UNIQUE
 );
@@ -23,3 +23,28 @@ CREATE TABLE user_roles (
 );
 
 CREATE INDEX idx_user_roles_role_id ON user_roles(role_id);
+
+CREATE TABLE projects (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(75),
+    description VARCHAR(350) NOT NULL,
+    createdAt TIMESTAMP  NOT NULL,
+    owner INT NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE INDEX idx_projects_user_id ON projects(owner);
+
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(75),
+    description VARCHAR(350) NOT NULL,
+    status VARCHAR(20) NOT NULL
+        CHECK (status IN ('TODO', 'IN_PROGRESS', 'DONE')),
+    project INT NOT NULL REFERENCES projects(id) ON DELETE CASCADE,
+    assignedTo INT REFERENCES users(id) ON DELETE SET NULL,
+    createdAt TIMESTAMP NOT NULL,
+    updatedAt TIMESTAMP NOT NULL
+);
+
+CREATE INDEX idx_tasks_project ON tasks(project);
+CREATE INDEX idx_tasks_assignedTo ON tasks(assignedTo);
