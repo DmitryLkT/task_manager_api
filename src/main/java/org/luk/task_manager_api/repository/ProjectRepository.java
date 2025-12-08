62028edb77fcf2b065ea43dbede6efea5b2485b4
DmitryTL <Dmitry.plus1@yandex.ru>
2025-12-08 15:51:01 +0600

    fix project repos

--- src/main/java/org/luk/task_manager_api/repository/ProjectRepository.java
+++ src/main/java/org/luk/task_manager_api/repository/ProjectRepository.java
@@ -1,14 +1,18 @@
 package org.luk.task_manager_api.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.luk.task_manager_api.model.Project;
+import org.luk.task_manager_api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
   Optional<Project> findByTitle(String title);
+
+  List<Project> findByOwner(User owner);
 
   boolean existsByTitle(String title);
 }
