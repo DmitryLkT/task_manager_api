22e5c865700fe12ec158c89cf24607ca185aa5e7
DmitryTL <Dmitry.plus1@yandex.ru>
2025-12-08 15:46:39 +0600

    fix project response and request

--- src/main/java/org/luk/task_manager_api/dto/ProjectRequest.java
+++ src/main/java/org/luk/task_manager_api/dto/ProjectRequest.java
@@ -6,11 +6,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProjectRequest {
@@ -18,9 +18,9 @@ public class ProjectRequest {
   @Size(min=1, max=75, message = "Название проекта может содержать от 1 до 75 символов")
   @Schema(description = "Название проекта", example = "task_manager_api")
   @NotBlank(message = "Не может быть пустым")
-  private final String title;
+  private String title;
 
   @Size(min=1, max=500, message = "Описание проекта может содержать от 1 до 500 символов")
   @Schema(description = "Описание проекта")
-  private final String description;
+  private String description;
 }
--- src/main/java/org/luk/task_manager_api/dto/ProjectResponse.java
+++ src/main/java/org/luk/task_manager_api/dto/ProjectResponse.java
@@ -2,6 +2,8 @@ package org.luk.task_manager_api.dto;
 
 import java.time.LocalDateTime;
 
+import org.luk.task_manager_api.model.Project;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
@@ -15,4 +17,14 @@ public class ProjectResponse {
   private String description;
   private LocalDateTime createdAt;
   private String ownerName;
+
+  public static ProjectResponse fromEntity(Project project) {
+    return ProjectResponse.builder()
+              .id(project.getId())
+              .title(project.getTitle())
+              .description(project.getDescription())
+              .createdAt(project.getCreatedAt())
+              .ownerName(project.getOwner().getName())
+              .build();
+  }
 }
