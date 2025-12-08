f8d75e984e3e961d4c903c7ca2b1432d5a0611db
DmitryTL <Dmitry.plus1@yandex.ru>
2025-12-08 13:18:29 +0600

    add task repository

---
+++ src/main/java/org/luk/task_manager_api/repository/TaskRepository.java
@@ -0,0 +1,12 @@
+package org.luk.task_manager_api.repository;
+
+import java.util.Optional;
+
+import org.luk.task_manager_api.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+  Optional<Task> findByTitle(String title);
+}
