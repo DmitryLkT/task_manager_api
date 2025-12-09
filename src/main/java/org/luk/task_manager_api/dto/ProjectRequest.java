3bdd17a963eaa86315453ccc1c2668d15253af81
DmitryTL <Dmitry.plus1@yandex.ru>
2025-12-09 14:14:10 +0600

    added a check for the correct title in the dto

--- src/main/java/org/luk/task_manager_api/dto/ProjectRequest.java
+++ src/main/java/org/luk/task_manager_api/dto/ProjectRequest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,6 +19,8 @@ public class ProjectRequest {
   @Size(min=1, max=75, message = "Название проекта может содержать от 1 до 75 символов")
   @Schema(description = "Название проекта", example = "task_manager_api")
   @NotBlank(message = "Не может быть пустым")
+  @Pattern(regexp = "^[a-zA-Zа-яА-ЯёЁ0-9\\\\s_\\\\-.,!?()\\\"']+$",
+    message = "Допустимы буквы, цифры, пробелы и основные знаки препинания")
   private String title;
 
   @Size(min=1, max=500, message = "Описание проекта может содержать от 1 до 500 символов")
