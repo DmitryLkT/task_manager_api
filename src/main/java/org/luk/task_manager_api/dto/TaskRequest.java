634498a9d4628f894c2d7a217d167cc7d2d6954d
DmitryTL <Dmitry.plus1@yandex.ru>
2025-12-09 14:53:02 +0600

    add task request

---
+++ src/main/java/org/luk/task_manager_api/dto/TaskRequest.java
@@ -0,0 +1,29 @@
+package org.luk.task_manager_api.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class TaskRequest {
+
+  @Size(min=1, max=75, message = "Название задачи может содержать от 1 до 75 символов")
+  @Schema(description = "Название задачи", example = "add methods")
+  @NotBlank(message = "Не может быть пустым")
+  @Pattern(regexp = "^[a-zA-Z0-9]+[a-zA-Z0-9_-]*[a-zA-Z0-9]+$",
+    message = "Разрешены английские буквы, цифры, дефис и подчеркивание в середине")
+  private String title;
+
+  @Size(min=1, max=350, message = "Описание задачи может содержать от 1 до 350 символов")
+  @Schema(description = "Описание задачи")
+  private String description;
+}
