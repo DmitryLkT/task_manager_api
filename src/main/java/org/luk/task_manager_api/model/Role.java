e3a0cf8c22f3c54c738090de18ecff449f457fd3
DmitryTL <Dmitry.plus1@yandex.ru>
2025-12-17 16:23:33 +0600

    fix model role

--- src/main/java/org/luk/task_manager_api/model/Role.java
+++ src/main/java/org/luk/task_manager_api/model/Role.java
@@ -1,14 +1,9 @@
 package org.luk.task_manager_api.model;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Column;
 
@@ -31,9 +26,6 @@ public class Role {
 
   @Column(name="name", nullable=false)
   private String name;
-  
-  @ManyToMany(mappedBy="roles", fetch=FetchType.LAZY)
-  private Set<User> users = new HashSet<>();
 
   public Role(String name) {
     this.name = name;
