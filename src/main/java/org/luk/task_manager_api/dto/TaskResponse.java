2a2527d2b69fb9632353c0bc71e7c39c5c5d61c6
DmitryTL <Dmitry.plus1@yandex.ru>
2025-12-17 16:27:42 +0600

    added constructors

--- src/main/java/org/luk/task_manager_api/dto/TaskResponse.java
+++ src/main/java/org/luk/task_manager_api/dto/TaskResponse.java
@@ -6,11 +6,15 @@ import org.luk.task_manager_api.model.Status;
 import org.luk.task_manager_api.model.Task;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Schema(description="Ответ с данными задачи")
 public class TaskResponse {
   private Long id;
