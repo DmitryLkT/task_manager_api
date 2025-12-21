204d4e6e1775b7b2d5ce50b67c71bd9197f915ab
DmitryTL <Dmitry.plus1@yandex.ru>
2025-12-21 13:02:51 +0600

    fix Project Not Found Exception

--- src/main/java/org/luk/task_manager_api/exception/customExceptions/ProjectNotFoundException.java
+++ src/main/java/org/luk/task_manager_api/exception/customExceptions/ProjectNotFoundException.java
@@ -4,4 +4,8 @@ public class ProjectNotFoundException extends RuntimeException {
   public ProjectNotFoundException(String message) {
     super(String.format("Project not found %s", message));
   }
+
+  public ProjectNotFoundException(Long message) {
+    super(String.format("Project not found %d", message));
+  }
 }
